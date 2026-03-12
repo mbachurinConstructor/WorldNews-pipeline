1a7675416025b36e322c6a2d1b6a51dc527f19e8
Mark Bachurin <mark.bachurin@jetbrains.com>
2026-03-12 22:32:41 +0100

    run open_db_container_terminal command from main directory, export .env variables into shell session

--- bash_scripts/open_db_container_termninal.sh
+++ bash_scripts/open_db_container_termninal.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker exec -it worldnews-postgres psql -U ${POSTGRES_USER} -d ${POSTGRES_DB}
+PROJECT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+cd "$PROJECT_DIR"
+
+export $(grep -v '^#' .env | xargs)
+
+docker exec -it worldnews-pipeline-news-postgres-1 psql -U $POSTGRES_USER -d $POSTGRES_DB
