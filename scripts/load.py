0dc4c8cf496221c118ac2bc5e58fae0e35ce8bfc
Mark Bachurin <mark.bachurin@jetbrains.com>
2026-03-13 01:28:54 +0100

    Add type safety checks in load

--- scripts/load.py
+++ scripts/load.py
@@ -2,6 +2,6 @@ from scripts.db import init_db, load_articles
 from aws.aws_download_from_s3 import download_from_s3
 
 def load_to_db() -> None:
-    data = download_from_s3("transformed/")
+    data : list = download_from_s3("transformed/")
     init_db()
     load_articles(data)
