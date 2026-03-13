7994ba2245c59cf7898f318e0871dd551e95501a
Mark Bachurin <mark.bachurin@jetbrains.com>
2026-03-13 01:17:10 +0100

    Add type safety checks in news_pipeline_dag

--- dags/news_pipeline_dag.py
+++ dags/news_pipeline_dag.py
@@ -10,7 +10,7 @@ from scripts.extract import run_extract
 from scripts.transform import run_transform
 from scripts.load import load_to_db
 
-default_args = {
+default_args : dict = {
     'owner': "airflow",
     "retries" : 1,
     "retry_delay" : timedelta(minutes=5),
@@ -24,17 +24,17 @@ with DAG(
     start_date=datetime(2026, 3, 12),
     catchup=False,
 ) as dag:
-    extract = PythonOperator(
+    extract : PythonOperator = PythonOperator(
         task_id="extract",
         python_callable=run_extract,
     )
 
-    transform = PythonOperator(
+    transform : PythonOperator = PythonOperator(
         task_id="transform",
         python_callable=run_transform,
     )
 
-    load = PythonOperator(
+    load : PythonOperator = PythonOperator(
         task_id="load_to_db",
         python_callable=load_to_db,
     )
